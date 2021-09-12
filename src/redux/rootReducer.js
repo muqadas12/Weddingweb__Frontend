0979815a1ab79ed9f0e8f1c67ef15f1aae0382fd
muqadas12 <60438885+muqadas12@users.noreply.github.com>
2021-09-12 11:13:58 +0500

    second commit

--- src/redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "../redux/Users/Reducer";
-const rootReducer=combineReducers({
-    userReducer:userReducer
-})
-
-export default rootReducer;
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-
-// import { configureStore } from '@reduxjs/toolkit'
-// import authReducer from "../redux/Reducers/authReducers"
-
-// export default configureStore({
-//   reducer: {
-//     user:authReducer
-//   }
-// })
