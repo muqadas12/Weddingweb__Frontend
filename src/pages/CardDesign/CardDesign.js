18cd661669612381baf6e5cbe4e8ac11839e2c39
muqadas12 <60438885+muqadas12@users.noreply.github.com>
2021-10-14 12:48:38 +0500

    card-design

--- src/pages/CardDesign/CardDesign.js
+++ src/pages/CardDesign/CardDesign.js
@@ -22,7 +22,13 @@ function ArrestWarrant() {
 export class Parent extends React.Component {
   constructor(props) {
     super(props);
-
+    this.handleChangeText = this.handleChangeText.bind(this);
+    this.handleChangeText2 = this.handleChangeText2.bind(this);
+    this.handleChangeTime = this.handleChangeTime.bind(this);
+    this.handleChangeType = this.handleChangeType.bind(this);
+    this.handleChangegender = this.handleChangegender.bind(this);
+    this.handleChangevenue = this.handleChangevenue.bind(this);
+    this.setSelectedImg = this.setSelectedImg.bind(this);
     this.state = {
       bride: 'Maryum',
       groom: 'Hassan',
@@ -33,46 +39,46 @@ export class Parent extends React.Component {
     };
   }
 
-  handleChangeText = (newText) => {
+  handleChangeText(newText) {
     this.setState({
       bride: newText,
     });
-  };
-  handleChangeText = (newText) => {
+  }
+  handleChangeText(newText) {
     this.setState({
       groom: newText,
     });
-  };
+  }
 
-  handleChangeTime = (newText) => {
+  handleChangeTime(newText) {
     this.setState({
       time: newText,
     });
-  };
+  }
 
-  handleChangeType = (newText) => {
+  handleChangeType(newText) {
     this.setState({
       type: newText,
     });
-  };
+  }
 
-  handleChangegender = (newText2) => {
+  handleChangegender(newText2) {
     this.setState({
       gender: newText2,
     });
-  };
+  }
 
-  handleChangevenue = (newText2) => {
+  handleChangevenue(newText2) {
     this.setState({
       venue: newText2,
     });
-  };
+  }
 
-  handleChangeText2 = (newText2) => {
+  handleChangeText2(newText2) {
     this.setState({
       text1: newText2,
     });
-  };
+  }
 
   render() {
     return (
